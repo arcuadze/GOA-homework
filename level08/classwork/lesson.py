cdce75e058a6a4943ab4caafba08cc24a07edf63
arcuadze <mariamiarcuadze@gmail.com>
2025-03-31 19:36:16 +0300

    commit

---
+++ level08/classwork/lesson.py
@@ -0,0 +1,28 @@
+num1=50
+num2=10
+mimateba = num1 + num2
+sxvaoba = num1 - num2
+print(mimateba)
+print(sxvaoba)
+#es aris intejeri
+
+num1=1.50
+num2=2.30
+sxvaoba = num2-num1
+print(sxvaoba)
+#es aris ploati
+
+num1="ma"
+num2="ri"
+print(num1+num2)
+
+num1=20
+num2=2
+num3=4
+num4=1
+sxvaoba=num1-num2-num3-num4
+print(sxvaoba)
+
+num1=1.50
+num2=2.30
+print(num1+num2)
