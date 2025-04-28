e2c9351fa5515d81272a3b1c4b012fcf9ed45468
arcuadze <mariamiarcuadze@gmail.com>
2025-04-28 15:20:56 +0300

    commit

---
+++ level10/homework/lesson.py
@@ -0,0 +1,75 @@
+num1= str(input("enter your name"))
+
+num2= str(input("enter your surname"))
+
+num3= str(input("enter your age"))
+
+num4= str(input("enter your frends name"))
+
+num5= str(input("enter your frends surname"))
+
+num6= str(input("entr your frends age"))
+
+num7= str(input("enter your password"))
+
+num8= str(input("enter your frends password"))
+
+num9= str(input("enter your email"))
+
+num10= str(input("enter your friends email"))
+             
+
+print(num1+num2+num3+num4+num5+num6+num7+num8+num9+num10)
+
+
+
+num1=int(input("enter three num"))
+
+num2=int(input("enter one num"))
+
+num3=int(input("enter four num"))
+
+num4=int(input("enter six num"))
+
+num5=int(input("enter ten num"))
+
+num6=int(input("enter seven num"))
+
+num7=int(input("enter two num"))
+
+num8=int(input("enter five num"))
+
+num9=int(input("enter nine num"))
+
+num10=int(input("enter eight num"))
+
+
+print(num1+num2+num3+num4+num5+num6+num7+num8+num9+num10)
+
+
+
+
+num1=float(input("enter one whole five" ))
+
+num2=float(input("enter three whole sixty"))
+
+num3=float(input("enter four whole fifty"))
+
+num4=float(input("enter six whole nine"))
+
+num5=float(input("enter seven whole eight"))
+
+num6=float(input("enter eight whole ninety"))
+
+num7=float(input("enter nine whole five"))
+
+num8=float(input("enter ten whole six"))
+
+num9=float(input("enter four whole two"))
+
+num10=float(input("enter one whole eight"))
+
+
+
+
+print(num1+num2+num3+num4+num5+num6+num7+num8+num9+num10)
